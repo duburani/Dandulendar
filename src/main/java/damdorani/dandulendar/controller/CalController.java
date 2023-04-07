cda4a71ae00fe7afc80a419a0a03e1e235127265
duburani <108318465+duburani@users.noreply.github.com>
2023-04-07 12:38:55 +0900

    레이아웃 설정 완료

--- src/main/java/damdorani/dandulendar/controller/CalController.java
+++ src/main/java/damdorani/dandulendar/controller/CalController.java
@@ -1,11 +1,11 @@
 package damdorani.dandulendar.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class CalController {
 
