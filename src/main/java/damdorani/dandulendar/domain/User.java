fa99b79ed152c34f465940f9e0ba1e696c37b950
duburani <108318465+duburani@users.noreply.github.com>
2023-05-15 09:49:56 +0900

    캘린더 저장

--- src/main/java/damdorani/dandulendar/domain/User.java
+++ src/main/java/damdorani/dandulendar/domain/User.java
@@ -3,15 +3,10 @@ package damdorani.dandulendar.domain;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 
